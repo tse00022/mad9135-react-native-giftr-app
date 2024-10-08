579330838a55a041d069921e15d23e45bb8eb68d
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-10-07 23:11:48 -0400

    Add style for peopleScreen

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -20,14 +20,15 @@ export default function PeopleScreen({navigation}) {
   ];
 
   return (
-    <View>
+    <View style={{padding: 10}}>
+        <Text style={{fontSize: 20}}>People List</Text>
         <FlatList
             data={people}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-            <View>
-                <Text>{item.name}</Text>
-                <Text>{item.dob}</Text>
+            <View style={{borderRadius: 5, marginVertical: 5, padding: 10, backgroundColor: "lightgrey"}}>
+                <Text style={{fontSize: 18}}>{item.name}</Text>
+                <Text style={{fontSize: 16, color: "darkgrey"}}>{item.dob}</Text>
             </View>
             )}
         />
